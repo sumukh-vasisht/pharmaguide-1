aef2a53e3dcc6548a860cd7aafa23d4664d1df21
sumukh-vasisht <sumukhmys1999@gmail.com>
2019-04-15 20:54:23 +0530

    Script for viewPatient

--- src/pages/viewPatient.vue
+++ src/pages/viewPatient.vue
@@ -1,6 +1,6 @@
 <template>
   <f7-page :name="'Patient '">
-    <f7-navbar :title="'Patient Details '+ id" back-link="Back">
+    <f7-navbar :title="'Patient Details - '+ id" back-link="Back">
       <f7-nav-right style="padding-right: 20px;">
           <f7-button raised fill  href="/">Home</f7-button>
         </f7-nav-right>
@@ -9,35 +9,35 @@
     <f7-block-title>Patient ID : </f7-block-title>
     <f7-list no-hairlines-md>
       <p></p>
-      <f7-block-title>{{ }}</f7-block-title>
+      <f7-block-title>{{id}}</f7-block-title>
     </f7-list>
 
     <f7-block-title>Patient Name : </f7-block-title>
     <f7-list no-hairlines-md>
       <p></p>
-      <f7-block-title >{{ }}</f7-block-title>
+      <f7-block-title >{{selected.patientName}}</f7-block-title>
     </f7-list>
     <f7-block-title>Age : </f7-block-title>
     <f7-list no-hairlines-md>
       <p></p>
-      <f7-block-title>{{ }}</f7-block-title>
+      <f7-block-title>{{selected.patientAge}}</f7-block-title>
     </f7-list>
     <f7-block-title>Gender : </f7-block-title>
     <f7-list no-hairlines-md>
       <p></p>
-      <f7-block-title>{{ }}</f7-block-title>
+      <f7-block-title>{{selected.patientGender}}</f7-block-title>
     </f7-list>
 
     <f7-block-title>Date of Birth : </f7-block-title>
     <f7-list no-hairlines-md>
       <p></p>
-      <f7-block-title>{{ }}</f7-block-title>
+      <f7-block-title>{{selected.patientDOB}}</f7-block-title>
     </f7-list>
 
-    <f7-block-title>Contact Number : </f7-block-title>
+    <f7-block-title>Emergency Contact Number : </f7-block-title>
     <f7-list no-hairlines-md>
       <p></p>
-      <f7-block-title>{{ }}</f7-block-title>
+      <f7-block-title>{{selected.patientEmergencyContact}}</f7-block-title>
     </f7-list>
     
     <!--<f7-block>      
@@ -46,14 +46,20 @@
   </f7-page>
 </template>
 <script>
+import firebase from '../js/firebaseInit';
+import functions from '../js/functions';
 export default {
   data(){
     return {
-      id : ""
+      id : "",
+      selected:{}
     }
   },
   beforeMount(){
     this.id = this.$f7route.params.id
+    console.dir(this.$f7router.url);
+    console.dir(this.$f7router);
+    functions.getDocument("patients",this,"selected")
   }
 }
 </script>
