ba4d421a339da9b427d8b2f2743aeb478cf1fc5c
amir <amirsyed8893@gmail.com>
2019-04-23 15:04:37 +0530

    minor changes

--- src/pages/patient/patientListDisease.vue
+++ src/pages/patient/patientListDisease.vue
@@ -1,14 +1,14 @@
 <template>
     <f7-page>
     <f7-navbar>
-      <f7-navbar title="Patients List" back-link="Back">
+      <f7-navbar title="Patient's disease list" back-link="Back">
         <f7-nav-right style="padding-right: 20px;">
           <f7-button raised fill  :href="'/patientHome/'+this.id">Home</f7-button>
         </f7-nav-right>
       </f7-navbar>
     </f7-navbar> 
     <f7-list>
-      <f7-list-item :link="'/patientMedicalHistory/'+id+'/'+disease" v-for="disease in documents" :title="disease" v-bind:key="disease"></f7-list-item>
+      <f7-list-item :link="'/patientMedicalHistory/'+id+'/'+disease" v-for="(disease,index) in documents" :title="(index+1)+ '. ' +disease" v-bind:key="index"></f7-list-item>
     </f7-list>
     </f7-page>   
 </template>
